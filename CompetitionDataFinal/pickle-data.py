690770e83e57630ef9f54572c55c5057ad267275
centipeda <joshua.c.guam@gmail.com>
2020-02-29 08:48:38 -0500

    adjust data

--- CompetitionDataFinal/pickle-data.py
+++ CompetitionDataFinal/pickle-data.py
@@ -6,12 +6,14 @@ import numpy as np
 file1 = 'impressions-train.csv'
 file2 = 'ratings-final.csv'
 file3 = 'movie-data.json'
+file4 = 'test.csv'
 
 def main(): 
-    with open(file1) as impressions, open(file2) as ratings, open(file3) as moviedata:
+    with open(file1) as impressions, open(file2) as ratings, open(file3) as moviedata, open(file4) as realTestData:
         moviedata = json.load(moviedata)
         impressionLines = [[int(a) for a in x.strip().split(',')] for x in impressions.readlines()[1:]]
         ratingLines = [[int(a) for a in x.strip().split(',')] for x in ratings.readlines()[1:]]
+        testLines = [[int(a) for a in x.strip().split(',')] for x in realTestData.readlines()[1:]]
 
         both = ratingLines + impressionLines
         # both = impressionLines
@@ -31,12 +33,27 @@ def main():
                 inputData.append(thisMovieData['genre_ids'][0])
             allInputData.append(inputData)
 
+        allTestData = []
+        for rating in testLines:
+            inputData = []
+            inputData = [rating[0], rating[1]]
+            movieID = rating[1]
+            thisMovieData = moviedata[str(movieID)]
+            inputData.append(thisMovieData['popularity'])
+            inputData.append(thisMovieData['vote_average'])
+            if len(thisMovieData['genre_ids']) == 0:
+                unGenred.add(thisMovieData['title'])
+            else:
+                inputData.append(thisMovieData['genre_ids'][0])
+            allTestData.append(inputData)
+
         # normalize
         # maxes = [max([a[x] for a in allInputData]) for x in range(len(allInputData[0]))]
         # for rating in allInputData:
         #     for attrIndex in range(len(rating)):
         #         rating[attrIndex] = rating[attrIndex]/maxes[attrIndex]
 
+        testIn = np.array(allTestData).astype(np.float32)
         funcIn = np.array(allInputData).astype(np.float32)
         funcOut = np.array([ b[2] for b in both]).astype(np.int64)
 
@@ -44,11 +61,9 @@ def main():
         print(funcIn.dtype)
         print(type(funcIn))
 
+        np.save('test.npy', allTestData)
         np.save('ratings-and-impressions-input.npy', funcIn)
         np.save('impressions-and-ratings-output.npy', funcOut)
 
-        print(both[0:4])
-        print(funcIn[0:4])
-        print(funcOut[0:4])
-
+        print(allTestData)
 main()
